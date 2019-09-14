027871c91ce2d432796f6915740da0084e341779
hemanthkr <hemanthkr30@gmail.com>
2019-09-14 17:04:50 +0530

    test cases

--- stock-market-company/src/test/java/com/cts/stockmarketcompany/StockMarketCompanyApplicationTests.java
+++ stock-market-company/src/test/java/com/cts/stockmarketcompany/StockMarketCompanyApplicationTests.java
@@ -1,16 +1,32 @@
 package com.cts.stockmarketcompany;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+import junit.framework.Assert;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class StockMarketCompanyApplicationTests {
 
 	@Test
-	public void contextLoads() {
-	}
+	public void testListofCompany() throws URISyntaxException {
+		RestTemplate restTemplate = new RestTemplate();
 
+		final String baseUrl = "http://localhost:8085/company";
+		URI uri = new URI(baseUrl);
+
+		ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
+
+		// Verify request succeed
+		Assert.assertEquals(200, result.getStatusCodeValue());
+		
+	}
 }
