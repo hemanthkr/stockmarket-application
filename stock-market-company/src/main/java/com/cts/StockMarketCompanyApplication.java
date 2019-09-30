8edbcf30a39d40b1c05f55a3b5480c40080c3004
Hemanth K R <44025240+hemanthkr@users.noreply.github.com>
2019-09-30 10:24:02 +0530

    Update StockMarketCompanyApplication.java

--- stock-market-company/src/main/java/com/cts/StockMarketCompanyApplication.java
+++ stock-market-company/src/main/java/com/cts/StockMarketCompanyApplication.java
@@ -1,5 +1,7 @@
 package com.cts;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -8,8 +10,12 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 public class StockMarketCompanyApplication {
 
+	private static final Logger LOGGER = LogManager.getLogger(StockMarketCompanyApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(StockMarketCompanyApplication.class, args);
 	}
+	
+	LOGGER.info("Welcome to StockMarket Application.");
 
 }
